4130fc14f681415ee1f3f8e0c4ced318a6fc15e4
Morchid Chellali <morchid.chellali98@gmail.com>
2022-01-10 16:43:18 +0100

    Good progress

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('pages/misc/about'));
+
+// ADS routes
+app.get('/ads', (req, res) => res.render('pages/ads/list'));
+
+// AUTH routes
+app.get('/login', (req, res) => res.render('pages/auth/login'));
+app.get('/signup', (req, res) => res.render('pages/auth/signup'));
+app.get('/forgot-password', (req, res) => res.render('pages/auth/forgot-password'));
+app.get('/resend-link', (req, res) => res.render('pages/auth/resend-link'));
+app.get('/reset-password', (req, res) => res.render('pages/auth/reset-password'));
+
+// ADMIN routes
+app.get('/admin/users', (req, res) => res.render('pages/admin/users'));
+app.get('/admin/users/:id', (req, res) => res.render('pages/admin/user'));
 
 const port = process.env.PORT || 3100;
 
